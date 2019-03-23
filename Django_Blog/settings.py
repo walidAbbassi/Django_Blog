1c6184515f0849bc1f9130be8245b453ac6f5170
abassi_ <walid.abbassi@esprit.tn>
2019-03-23 13:26:08 +0100

    add reset password user

--- Django_Blog/settings.py
+++ Django_Blog/settings.py
@@ -149,3 +149,11 @@ CKEDITOR_CONFIGS = {
         'forcePasteAsPlainText': True,
     },
 }
+
+# USER PASSWORD RESET
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'django@gmail.com'
+EMAIL_HOST_PASSWORD = 'djangoPassword'
